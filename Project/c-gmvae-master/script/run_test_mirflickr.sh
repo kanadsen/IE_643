2defbf6a81006e6df658738c992114532a9c7abc
kanadsen <kanadsen01@gmail.com>
2023-09-08 16:59:51 +0530

    Update

---
+++ Project/c-gmvae-master/script/run_test_mirflickr.sh
@@ -0,0 +1,1 @@
+python main.py --data_dir data/mirflickr/mirflickr_data.npy --test_idx data/mirflickr/mirflickr_test_idx.npy --label_dim 38 --z_dim 38 --feature_dim 1000 --nll_coeff 0.5 --c_coeff 0.0 --batch_size 64 --mode test --emb_size 2048 --reg gmvae -cp model/model_mirflickr/lr-0.002_lr-decay_0.50_lr-times_4.0_nll-0.50_l2-1.00_c-0.00/vae-6853
